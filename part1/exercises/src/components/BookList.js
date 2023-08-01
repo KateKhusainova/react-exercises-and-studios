1767e3292ae3ffd2a80765d2d017fa41f9a0bcd9
Ekaterina <katekhusainova8@gmail.com>
2023-08-01 18:28:47 -0400

    Modified the CSS. Added more movies. Completed the Booklist component

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "My Favotite Books";
+   let book1 = "https://pictures.abebooks.com/isbn/9781400079988-us.jpg";
+   let book2 = "https://m.media-amazon.com/images/I/51HSkTKlauL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg";
+   let book3 = "https://target.scene7.com/is/image/Target/GUEST_0d92d1a4-2ad5-4371-bbf5-670c4e9eaa4f?wid=1000&hei=1000&fit=constrain&qlt=80&fmt=webp";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="War and Piece by Leo Tolstoy" />
+         <img src={book2} alt="Harry Potter by J.K.Rowling" />
+         <img src={book3} alt="It by Stephen King" />
       </div>      
    );
 }

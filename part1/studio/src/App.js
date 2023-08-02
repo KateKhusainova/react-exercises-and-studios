83ce3629f4b203610df27ba340359fcc51020aa2
Ekaterina <katekhusainova8@gmail.com>
2023-08-02 18:24:21 -0400

    Created the RecipeIngredients component

--- part1/studio/src/App.js
+++ part1/studio/src/App.js
@@ -1,12 +1,14 @@
 //import logo from './logo.svg';
 import './App.css';
 import RecipeDescription from './components/Description.js'
+import RecipeIngredients from './components/Ingredients.js'
 
 function App() {
   return (
     <div className="App">
       <div>
       <RecipeDescription />
+      <RecipeIngredients />
       </div>
     </div>
   );

641bb22a11233eb825e21cc52a433edc53f82f21
Ekaterina <katekhusainova8@gmail.com>
2023-08-04 13:26:30 -0400

    Added Profile component step from A to D

--- part2/exercises-2/src/components/Profile.js
+++ part2/exercises-2/src/components/Profile.js
@@ -1,13 +1,22 @@
 import './styles.css';
 import Button from './Button';
+import oceans from './oceans.json';
 
+const listItem = oceans.map(ocean => 
+   <div key={ocean.id} className={`${ocean.fishCheck === "true" ? "isAFish" : "profile"}`}>
+      <img src={ocean.image} alt={ocean.name} className="img"/>
+      <h1>{ocean.name}</h1>
+   </div>
+);
 
 function Profile()
 {
    return(
       <>
-         <h3>Coming Soon! Profiles of Ocean creatures</h3>
-         <Button />
+      <ul>
+         {listItem}
+      </ul>
+      <Button />
       </>  
    );
 }
